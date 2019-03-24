614b1806b4ed08826178a18441e506b58a155b07
LywockeeZ <kofln@vip.qq.com>
2019-03-24 22:30:27 +0800

    Add Turn in Perspective Camera

--- SeaWorld/Assets/Scripts/MovementController.cs
+++ SeaWorld/Assets/Scripts/MovementController.cs
@@ -33,7 +33,15 @@ public class MovementController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            //如果是正交摄像机用以下代码
+            //mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            LayerMask mask = 1 << 10;
+            RaycastHit hitInfo;          
+            if (Physics.Raycast(ray, out hitInfo, 200, mask))
+            {
+                mousePos = hitInfo.point;
+            }
             Direction = mousePos - transform.position;
             isRotating = true;
         }
